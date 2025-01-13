7473bb70faca124991a506c319b5e67a6ea4d809
Aditya S Kelaskar <kelaskaraditya1@gmail.com>
2025-01-13 20:52:56 +0530

    Web security Configuration Completed

--- src/main/java/com/StarkIndustries/SpringSecurityMark1/Configuration/SecurityConfiguration.java
+++ src/main/java/com/StarkIndustries/SpringSecurityMark1/Configuration/SecurityConfiguration.java
@@ -1,4 +1,23 @@
 package com.StarkIndustries.SpringSecurityMark1.Configuration;
 
-public class SecurityConfiguration {
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {    
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)throws Exception{
+        httpSecurity.csrf(csrf->csrf.disable());
+        httpSecurity.authorizeHttpRequests(request->request.anyRequest().authenticated());
+        httpSecurity.formLogin(Customizer.withDefaults());
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
 }
